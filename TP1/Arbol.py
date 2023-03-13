35db4ec09091c0aee6cb1eed9ceefc7a0c9de17d
Juani1904 <juanicei99@gmail.com>
2023-03-13 17:06:44 -0300

    Search tree created by Breadth-First Search algorithm

--- TP1/Arbol.py
+++ TP1/Arbol.py
@@ -1,15 +1,61 @@
+from Nodos import Nodo
+
+
 class Arbol:
 
-    #Creamos el constructor del objeto Arbol, el cual sera la clase agregada, mientras que los objetos nodos son objetos componentes de dicha composicion
-    #Vamos a crear el arbol a partir del nodo Raiz
-    def __init__(self, nodoRaiz):
-        self.nodoRaiz = nodoRaiz
-        self.nodosVisitados = [nodoRaiz]
-        self.nodosNoVisitados = []
+    # Creamos el constructor del objeto Arbol, el cual sera la clase agregada, mientras que los objetos nodos son objetos componentes de dicha composicion
+    # Vamos a crear el arbol a partir del nodo Raiz
+    def __init__(self, numeroFilas, numeroColumnas):
+        #self.nodoRaiz = nodoRaiz
+        self.nodos = []
+        self.numeroFilas = numeroFilas
+        self.numeroColumnas = numeroColumnas
 
-    #Ahora creamos el metodo para crear el arbol a partir de nodo raiz
+        # Llamamos a los metodos de crearNodo y asignarVecinos
+        self.crearNodo()
+        self.asignarVecinos()
+        for nodo in self.nodos:
+            print(f"({nodo.coordenadaX},{nodo.coordenadaY})", end=" ")
+
+        print("Ahora los vecinos de cada nodo")
+        for nodo2 in self.nodos:
+            print(f"({nodo2.coordenadaX},{nodo2.coordenadaY})")
+            for nodo3 in self.nodos:
+                print("\n\nNodo "+str(self.nodos.index(nodo3))+'\n\n')
+                for vecino in nodo3.vecinos:
+                    
+                    print(f"({vecino.coordenadaX},{vecino.coordenadaY})", end=" ")
+
+    # Ahora creamos el metodo para crear el arbol a partir de nodo raiz
     '''
     Para ello vamos a utilizar el algoritmo de busqueda en anchura, para ir creando el arbol al instanciar los distintos objetos nodos,
     los cuales determinaran el espacio de busqueda donde luego aplicaremos nuestro algortimo A*
     '''
-    def crearArbol(self, nodoRaiz):
+    # En un principio solamente creamos los nodos con este metodo, luego con otro metodo asignaremos los vecinos a cada nodo
+
+    def crearNodo(self):
+        for filas in range(0, self.numeroFilas+1):
+            for columnas in range(0, self.numeroColumnas+1):
+                # Creamos la lista donde se guardaran todos los objetos nodo
+                self.nodos.append(Nodo(columnas, filas, False, None))
+
+    # Ahora creamos el metodo para asignar los vecinos a cada nodo
+    def asignarVecinos(self):
+
+        for nodo in self.nodos:
+            # Condiciones (tomando como eje x positivo la posicion hacia la derecha y como y positivo la posicion hacia abajo)
+            nodoDerecho = nodo.coordenadaX + 1, nodo.coordenadaY
+            nodoIzquierdo = nodo.coordenadaX - 1, nodo.coordenadaY
+            nodoArriba = nodo.coordenadaX, nodo.coordenadaY - 1
+            nodoAbajo = nodo.coordenadaX, nodo.coordenadaY + 1
+            
+            # Como no me interesan los vecinos diagonales, solamente verticales u horizontales, utilizo el operador logico XOR o OR exclusivo
+            # Para poder asignar los valores creo un seeker o buscador que recorra la lista de nodos y me devuelva el nodo que cumpla con las condiciones
+            for seeker in self.nodos:
+                #Coordenadas del nodo actual del seeker (en una tupla)
+                nodoActual = seeker.coordenadaX, seeker.coordenadaY
+                if ((nodoActual == nodoDerecho) or (nodoActual == nodoIzquierdo) or (nodoActual == nodoArriba) or (nodoActual == nodoAbajo)):
+                    nodo.vecinos.append(seeker)
+
+
+arbol = Arbol(5, 5)
