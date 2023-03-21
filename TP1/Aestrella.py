925a1e33f7b613ed41fdd97e776eeb0778184a8e
Juani1904 <juanicei99@gmail.com>
2023-03-20 21:26:49 -0300

    Code fixed, working

--- TP1/Aestrella.py
+++ TP1/Aestrella.py
@@ -35,15 +35,13 @@ class Aestrella:
         Tenemos que hayar la forma de recorrer el arbol que creamos mediante el objeto arbol, calcular el g,h y f de cada nodo 
         y luego ir comparando los valores de f para ir eligiendo el nodo con menor f
         '''
-        numeroIteraciones=0 #Contador de iteraciones
+        self.numeroIteraciones=0 #Contador de iteraciones
         # Para ello primero debemos establecer la condicion de satisfaccion, es decir, cuando nuestro encuentra el test objeto, o nodo final
         while (self.nodoActual != self.nodoFinal):
-            numeroIteraciones+=1 #Aumentamos el contador en 1
+            self.numeroIteraciones+=1 #Aumentamos el contador en 1
             #Generamos el try
             try:
-                #Si el numero de iteraciones es mayor a 1000, lanzamos la excepcion
-                if numeroIteraciones>1000:
-                    raise nodosException("El buscador se quedo trabado.Haciendo backtrack al estado anterior")
+                
                 # Calculamos la funcion F de los vecinos del nodo actual
                 # Colocamos un valor muy alto para que el primer vecino que se encuentre tenga menor F
                 valorFmin = 100000
@@ -56,11 +54,9 @@ class Aestrella:
                         # Calculamos funcion F
                         vecino.funcionF = self.calculaF(vecino)
                         # Elegimos al nodo con menor F
-                        if (vecino.funcionF <= valorFmin):
+                        if (vecino.funcionF < valorFmin):
                             valorFmin = vecino.funcionF
                             self.vecinoFmin = vecino
-                        # Establecemos al vecino analizado como visado
-                        vecino.estado = True
 
                     else:
                         None
@@ -69,18 +65,34 @@ class Aestrella:
                 self.ruta(self.nodoActual.coordenadaX, self.nodoActual.coordenadaY,self.vecinoFmin.coordenadaX, self.vecinoFmin.coordenadaY)
                 # Actualizamos nodo actual al vecino con menor F y lo agregamos al camino
                 self.camino.append(self.vecinoFmin)
+                # Establecemos al vecino analizado como visado
+                self.nodoActual.vecinos[int(self.nodoActual.vecinos.index(self.vecinoFmin))].estado = True
+                #Elegimos al nuevo nodo actual
                 self.nodoActual = self.vecinoFmin
+                
+                 
+                #Si el numero de iteraciones es mayor a 1000, lanzamos la excepcion
+                if self.numeroIteraciones>1000:
+                    raise nodosException("El buscador se quedo trabado.Haciendo backtrack al estado anterior")
             
             except nodosException as e:
                 print(e) #Mostramos mensaje que el buscador se quedo trabado
-                #Seteamos este nodo actual como ya visaado para que no se vuelva a ejecutar
+                #Eliminamos el ultimo nodo del camino, el cual es el nodoActual
+                #Habra que eliminarlo varias veces porque se sumo varias veces al camino
+                #Para asegurarnos de que no siga quedando el mismo nodo en el camino, lo removemos de cualquier espacio donde pueda estar presente
+                for elemento in self.camino:
+                    if elemento==self.nodoActual:
+                        self.camino.remove(elemento)
+                #Establecemos los nodos vecinos del nodo de donde heredamos como no visitados
+                for vecino in self.camino[-1].vecinos:
+                    vecino.estado=False
+                #Seteamos al nodo actual como visitado, para que no se vuelva a ejecutar
                 self.nodoActual.estado=True
-                #Eliminamos el ultimo nodo del camino, el cual es este mismo nodo
-                self.camino.pop()
                 #Hacemos backtrack al nodo de donde vinimos, que es el ultimo nodo del camino
                 self.nodoActual=self.camino[-1]
                 #Seteamos al contador de iteraciones nuevamente en 0
-                numeroIteraciones=0
+                self.numeroIteraciones=0
+                continue
                 
 
         #Asignamos la distancia recorrida al atributo
@@ -90,6 +102,7 @@ class Aestrella:
         self.ruta(self.nodoActual.coordenadaX, self.nodoActual.coordenadaY,self.vecinoFmin.coordenadaX, self.vecinoFmin.coordenadaY)
 
         return f"El camino es: {self.instrucciones} y la distancia es {self.distanciaRecorrida} casillas"
+
     
     #Definimos metodo de reseteo de valores para poder iniciar una nueva busqueda sin problema
     def reset(self):
